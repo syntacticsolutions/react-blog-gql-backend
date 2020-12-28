92b1a3418f7d99b4eb25c02e4d92ab9a046d1523
Miguel Coder <syntacticsolutions@gmail.com>
2020-12-28 10:07:36 -0800

    Part 11 commit

---
+++ database/actions/run-migration.js
@@ -0,0 +1,42 @@
+const knex = require('../mysql')
+const promise = require('bluebird')
+const path = require('path')
+const fs = require('fs')
+const db = knex.client.config.connection.database || null
+
+function replaceAll (str, delimiter, replacement) {
+    return str.split(delimiter).join(replacement)
+}
+
+fs.readdir(
+    path.resolve(
+        __dirname,
+        '../migrations'
+    ),
+    'utf-8',
+    (err, files) => {
+        if(err) return console.log(err)
+        return promise.each(files, (fileName) => {
+            return new Promise((resolve, reject) => {
+                fs.readFile(
+                    path.resolve(
+                        __dirname,
+                        `../migrations/${fileName}`
+                    ),
+                    'utf-8',
+                    (err, sql) => {
+                        if (err) reject(err)
+                        return knex.raw(replaceAll(sql, '{}', db))
+                        .then(resolve)
+                        .catch((err) => {console.log(err)})
+                    }
+                )
+            })
+
+        })
+        .then(() => console.log('Migrations have run successfully.'))
+        .catch(err => {
+            console.log(err)
+        })
+    }
+)
---
+++ schemas/posts/index.js
@@ -0,0 +1,59 @@
+const fs = require('fs')
+const path = require('path')
+const PostService = require('../../api/posts')
+const {errorHandler} = require('../../api/utils')
+
+const getPostsWithAuthors = cb => 
+    async (parent, args, context, info) => {
+        const posts = await cb(args, parent)
+            .catch(errorHandler)
+
+        if (!posts.length) return []
+
+        const authors = await PostService.getPostsAuthors(
+            [...new Set(posts.map(({author_id}) => author_id))]
+        )
+        .catch(errorHandler)
+
+
+        const authorMap = authors.reduce((map, author) => ({
+            ...map,
+            [author.id]: `${author.first_name} ${author.last_name}`
+        }), {})
+
+        return posts.map(({categories, author_id, ...rest}, index) => 
+            ({
+                categories: categories.split(','),
+                ...rest,
+                author: authorMap[author_id]
+            })
+
+        )
+    }
+
+module.exports = {
+    resolvers: {
+        Query: {
+            getPostsByType: getPostsWithAuthors(
+                async ({type}) => await PostService.getPosts(type)
+            ),
+            getPostsByCategories: getPostsWithAuthors(
+                async ({cat_ids}) => await PostService.getPosts('default', cat_ids)
+            ),
+            getPost: async (parent, {id}) => {
+                const post = await PostService.getPost(id)
+
+                return post
+            }
+        },
+        Mutation: {
+            createPost: async (parent, args) => await PostService.createPost(args)
+        }
+    },
+    schema: fs.readFileSync(
+        path.resolve(
+            __dirname,
+            './posts-schema.graphql'
+        )
+    ).toString()
+}
